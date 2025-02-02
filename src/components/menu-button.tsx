3ca8ace17387d9a54379efede1c5ceaaa55d1cfa
fuerte <leandrofuerte75@gmail.com>
2025-02-02 18:41:03 +0100

    Creating close navBar function

--- src/components/menu-button.tsx
+++ src/components/menu-button.tsx
@@ -1,10 +1,36 @@
-import { CgMenuLeft } from "react-icons/cg";
+"use client";
+
+import { CgClose, CgMenuLeft } from "react-icons/cg";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 
 function MenuButton() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <CgMenuLeft size={30} className="cursor-pointer" />
-      <div className="absolute top-0 left-0 w-[80%] h-screen z-10 bg-background/90 border-r-2 border-background/80 backdrop-blur-sm" />
+      <button onClick={handleClick}>
+        <CgMenuLeft size={30} className="cursor-pointer" />
+      </button>
+      <AnimatePresence mode="wait">
+        {isOpen && (
+          <motion.div
+            transition={{ duration: 0.5 }}
+            initial={{ x: "-100%" }}
+            animate={{ x: "0" }}
+            exit={{ x: "-100%" }}
+            className="absolute top-0 left-0 w-[80%] h-screen z-10 bg-background/90 border-r-2 border-background/80 backdrop-blur-sm"
+          >
+            <button onClick={handleClick}>
+              <CgClose size={25} />
+            </button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 }
