82b6f15250d894c4f4e771c29c384b5e648c2e42
fuerte <leandrofuerte75@gmail.com>
2025-02-02 16:38:03 +0100

    Stlyling principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ExerciseCard from "@/components/exercise-card";
 import { Zen_Dots } from "next/font/google";
 
 const zendots = Zen_Dots({
@@ -7,10 +8,21 @@ const zendots = Zen_Dots({
 
 export default function Home() {
   return (
-    <main className={`${zendots.className} bg-background h-screen`}>
+    <main
+      className={`${zendots.className} bg-background h-screen flex flex-col items-center justify-center gap-2`}
+    >
       <h1 className="text-primary text-3xl font-bold text-center">
-        Gainz Journal
+        Gainz<span className="block text-primary">Journal</span>
       </h1>
+      <div className="text-secondary">
+        <h4 className="mb-2">Last added:</h4>
+        {/* Mapping over the array of exercises */}
+        <ul className="flex flex-col gap-2">
+          <ExerciseCard />
+          <ExerciseCard />
+          <ExerciseCard />
+        </ul>
+      </div>
     </main>
   );
 }
