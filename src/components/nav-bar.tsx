a0e4a27a9ab9876d609a48d7bd84016f2911db25
fuerte <leandrofuerte75@gmail.com>
2025-02-02 18:45:19 +0100

    Changing icon

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,11 +1,11 @@
-import { CgHome } from "react-icons/cg";
+import { CgHomeAlt } from "react-icons/cg";
 import MenuButton from "./menu-button";
 
 function NavBar() {
   return (
     <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary">
       <MenuButton />
-      <CgHome size={25} className="cursor-pointer" />
+      <CgHomeAlt size={25} className="cursor-pointer" />
     </nav>
   );
 }
