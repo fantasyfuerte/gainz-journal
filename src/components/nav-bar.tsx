df2f3b89fcf943ac886b97e556c95d259504bf0f
fuerte <leandrofuerte75@gmail.com>
2025-02-03 07:52:22 +0100

    Adding add exercise button

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -5,7 +5,9 @@ function NavBar() {
   return (
     <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary">
       <MenuButton>
-        <></>
+        <div className="flex flex-col gap-2 py-3 px-2">
+        <button className="bg-button rounded-lg py-2">New Exercise</button>
+        </div>
       </MenuButton>
       <CgHomeAlt size={25} className="cursor-pointer" />
     </nav>
