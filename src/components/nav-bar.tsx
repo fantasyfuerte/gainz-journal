ec1d753987595e6dfc0de8c92af56c656a20131a
fuerte <leandrofuerte75@gmail.com>
2025-02-03 13:28:48 +0100

    Creating /new route

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,15 +1,18 @@
 import { CgHomeAlt } from "react-icons/cg";
 import MenuButton from "./menu-button";
 import ExerciseCard from "./exercise-card";
+import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary">
+    <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary bg-background">
       <MenuButton>
         <div className="flex flex-col gap-3 py-3 px-2">
-          <button className="bg-button rounded-lg py-2 my-3">
-            New Exercise
-          </button>
+          <Link href="/new" className="flex flex-col">
+            <button className="bg-button rounded-lg py-2 my-3">
+              New Exercise
+            </button>
+          </Link>
           {/* Mapping over the array of exercises */}
           <ul className="flex flex-col gap-3">
             <ExerciseCard />
