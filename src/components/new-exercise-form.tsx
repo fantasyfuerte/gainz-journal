75653c7e73f00575fc98c3bfe5f22ecc15e8fa7d
fuerte <leandrofuerte75@gmail.com>
2025-02-06 11:51:00 +0100

    Componentizing select-component

--- src/components/new-exercise-form.tsx
+++ src/components/new-exercise-form.tsx
@@ -1,7 +1,6 @@
-"use client";
+import SelectComponent from "./select-component";
 
 // import { useState } from "react";
-import Select from "react-select";
 
 function NewExerciseForm() {
   const exerciseDescription =
@@ -18,50 +17,7 @@ function NewExerciseForm() {
 
   return (
     <form className="flex flex-col gap-5 items-start">
-      <Select
-        styles={{
-          control: (provided) => ({
-            ...provided,
-            backgroundColor: "transparent",
-            border: "none",
-            borderRadius: "0.5rem",
-            padding: "0.5rem",
-            fontSize: "1rem",
-            color: "#94a1b2",
-            outline: "none",
-          }),
-          menu: (provided) => ({
-            ...provided,
-            backgroundColor: "rgba(31, 18, 53,0.5)",
-            backdropFilter: "blur(1px)",
-            border: "none",
-            borderRadius: "0.5rem",
-          }),
-          option: (provided) => ({
-            ...provided,
-            backgroundColor: "transparent",
-            border: "none",
-            borderRadius: "0.5rem",
-            color: "#94a1b2",
-            fontWeight: "500",
-          }),
-          singleValue: (provided) => ({
-            ...provided,
-            color: "#fffffe",
-            fontWeight: "600",
-            fontSize: "20px",
-          }),
-          placeholder: (provided) => ({
-            ...provided,
-            color: "#94a1b2",
-            fontWeight: "600",
-            opacity: "0.8",
-          }),
-        }}
-        className="w-full"
-        isClearable
-        options={arr}
-      />
+      <SelectComponent options={arr} />
       <textarea
         className="bg-transparent text-secondary/90 outline-none placeholder:text-secondary/90 first-line:ml-5 w-full"
         placeholder={exerciseDescription}
