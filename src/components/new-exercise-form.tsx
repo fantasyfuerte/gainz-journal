61834ae73ed9b3d655aaac6244829b6bacfaf6b0
fuerte <leandrofuerte75@gmail.com>
2025-02-06 11:38:30 +0100

    Creating and basic styling react selection with react select

--- src/components/new-exercise-form.tsx
+++ src/components/new-exercise-form.tsx
@@ -1,43 +1,71 @@
 "use client";
 
-import { useState } from "react";
-import { muscles } from "@/../data";
+// import { useState } from "react";
+import Select from "react-select";
 
 function NewExerciseForm() {
   const exerciseDescription =
     "Add a description for your exercise and then press the save button.";
 
-  const [muscle, setMuscle] = useState("");
-  const [exercise, setExercise] = useState("");
+  //   const [muscle, setMuscle] = useState("");
+  //   const [exercise, setExercise] = useState("");
+
+  const arr = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   return (
     <form className="flex flex-col gap-5 items-start">
-      <select
-        className="bg-transparent text-secondary text-xl font-semibold outline-none placeholder:text-secondary w-full"
-        value={exercise}
-        onChange={(e) => setExercise(e.target.value)}
-      >
-        <option>Select exercise</option>
-        {muscle !== "Select muscle" &&
-          muscle !== "" &&
-          muscles
-            .filter((m) => m.name == muscle)[0]
-            .exercises.map((ex) => <option key={ex.name}>{ex.name}</option>)}
-      </select>
-      <select
-        className="bg-transparent text-secondary/90 outline-none placeholder:text-secondary/90 w-full"
-        value={muscle}
-        onChange={(e) => setMuscle(e.target.value)}
-      >
-        <option>Select muscle</option>
-        {muscles.map((muscle) => (
-          <option key={muscle.name}>{muscle.name}</option>
-        ))}
-      </select>
+      <Select
+        styles={{
+          control: (provided) => ({
+            ...provided,
+            backgroundColor: "transparent",
+            border: "none",
+            borderRadius: "0.5rem",
+            padding: "0.5rem",
+            fontSize: "1rem",
+            color: "#94a1b2",
+            outline: "none",
+          }),
+          menu: (provided) => ({
+            ...provided,
+            backgroundColor: "rgba(31, 18, 53,0.5)",
+            backdropFilter: "blur(1px)",
+            border: "none",
+            borderRadius: "0.5rem",
+          }),
+          option: (provided) => ({
+            ...provided,
+            backgroundColor: "transparent",
+            border: "none",
+            borderRadius: "0.5rem",
+            color: "#94a1b2",
+            fontWeight: "500",
+          }),
+          singleValue: (provided) => ({
+            ...provided,
+            color: "#fffffe",
+            fontWeight: "600",
+            fontSize: "20px",
+          }),
+          placeholder: (provided) => ({
+            ...provided,
+            color: "#94a1b2",
+            fontWeight: "600",
+            opacity: "0.8",
+          }),
+        }}
+        className="w-full"
+        isClearable
+        options={arr}
+      />
       <textarea
+        className="bg-transparent text-secondary/90 outline-none placeholder:text-secondary/90 first-line:ml-5 w-full"
         placeholder={exerciseDescription}
         aria-multiline="true"
-        className="bg-transparent text-secondary/90 outline-none placeholder:text-secondary/90 first-line:ml-5 w-full"
       />
       <button className="bg-button text-lg text-primary font-bold rounded-lg py-2 px-4 mt-12 self-end">
         Save
