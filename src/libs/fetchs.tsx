8444ce52eeb73122bd4ffbd1b658206d34afeacc
fuerte <leandrofuerte75@gmail.com>
2025-02-17 14:11:36 +0100

    Creating loadExercise function

--- src/libs/fetchs.tsx
+++ src/libs/fetchs.tsx
@@ -3,3 +3,9 @@ export async function loadExercises() {
     const data = await response.json();
     return data
   }
+
+  export async function loadExercise(id: number) {
+    const response = await fetch(`http://localhost:3000/api/exercises/${id}`);
+    const data = await response.json();
+    return data
+  }
