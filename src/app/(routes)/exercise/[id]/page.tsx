58a7ab6cf3d3075887d0beabfee9d8d4101bb195
fuerte <leandrofuerte75@gmail.com>
2025-02-18 09:26:27 +0100

    Adding description

--- src/app/(routes)/exercise/[id]/page.tsx
+++ src/app/(routes)/exercise/[id]/page.tsx
@@ -4,6 +4,7 @@ import { loadExercise } from "@/libs/fetchs";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+
 function Capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
@@ -12,7 +13,7 @@ function ExercisePage() {
   const [exercise, setExercise] = useState({
     name: "",
     description: "",
-    muscles: [],
+    createdAt: ""
   });
 
   const { id } = useParams();
@@ -26,9 +27,12 @@ function ExercisePage() {
 
   return (
     <main className="bg-background h-screen pt-16 px-4">
-      <h1 className="text-primary text-xl font-bold">
+     <h1 className="text-xl font-bold text-primary">
         {exercise.name.split(" ").map(Capitalize).join(" ")}
       </h1>
+      <p>{exercise.createdAt}</p>
+      <p className="text-secondary/75 mt-2 text-sm mr-5">{exercise.description}</p>
+      <h2 className="text-primary/80 text-lg font-semibold mt-8">Workouts</h2>
     </main>
   );
 }
