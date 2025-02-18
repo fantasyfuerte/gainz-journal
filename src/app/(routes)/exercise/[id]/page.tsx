eb30064c61724ca97277e92ceb6201148c848e3f
fuerte <leandrofuerte75@gmail.com>
2025-02-18 10:26:25 +0100

    Making textarea works and styling date

--- src/app/(routes)/exercise/[id]/page.tsx
+++ src/app/(routes)/exercise/[id]/page.tsx
@@ -4,7 +4,6 @@ import { loadExercise } from "@/libs/fetchs";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-
 function Capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
@@ -13,7 +12,7 @@ function ExercisePage() {
   const [exercise, setExercise] = useState({
     name: "",
     description: "",
-    createdAt: ""
+    createdAt: "",
   });
 
   const { id } = useParams();
@@ -27,11 +26,17 @@ function ExercisePage() {
 
   return (
     <main className="bg-background h-screen pt-16 px-4">
-     <h1 className="text-xl font-bold text-primary">
+      <h1 className="text-xl font-bold text-primary">
         {exercise.name.split(" ").map(Capitalize).join(" ")}
       </h1>
-      <p>{exercise.createdAt}</p>
-      <p className="text-secondary/75 mt-2 text-sm mr-5">{exercise.description}</p>
+      <p className="text-secondary/75 mt-2 text-sm">{exercise.createdAt}</p>
+      <textarea
+        className="text-secondary/75 mt-2 text-sm pr-5 bg-transparent w-full outline-none"
+        value={exercise.description}
+        onChange={(e) =>
+          setExercise({ ...exercise, description: e.target.value })
+        }
+      ></textarea>
       <h2 className="text-primary/80 text-lg font-semibold mt-8">Workouts</h2>
     </main>
   );
