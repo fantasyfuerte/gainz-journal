4a15beea18160dc1c89465f284ce534b07b6e2ea
fuerte <leandrofuerte75@gmail.com>
2025-02-18 06:49:07 +0100

    Getting url from process.env and using it in fetchss

--- src/libs/fetchs.tsx
+++ src/libs/fetchs.tsx
@@ -1,11 +1,16 @@
+const API_BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    : "https://tu-dominio.com";
+
 export async function loadExercises() {
-  const response = await fetch("http://localhost:3000/api/exercises");
+ const response = await fetch(`${API_BASE_URL}/api/exercises`);
   const data = await response.json();
   return data;
 }
 
 export async function loadExercise(id: number) {
-  const response = await fetch(`http://localhost:3000/api/exercises/${id}`);
+  const response = await fetch(`${API_BASE_URL}/api/exercises/${id}`);
   const data = await response.json();
   return data;
 }
