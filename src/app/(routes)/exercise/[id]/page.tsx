5e7fd85cdb869501a00a9d47fb85e676de66ef70
fuerte <leandrofuerte75@gmail.com>
2025-02-19 07:11:25 +0100

    Conditional rendering page and putting a loading icon

--- src/app/(routes)/exercise/[id]/page.tsx
+++ src/app/(routes)/exercise/[id]/page.tsx
@@ -3,19 +3,15 @@
 import { loadExercise } from "@/libs/fetchs";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { CgDisc } from "react-icons/cg";
+import { type Exercise } from "@/components/exercise-list";
 
 function Capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 function ExercisePage() {
-  const [exercise, setExercise] = useState({
-    name: "",
-    description: "",
-    createdAt: "",
-    date: "",
-    time: "",
-  });
+  const [exercise, setExercise] = useState<null | Exercise>(null);
 
   const { id } = useParams();
 
@@ -28,19 +24,32 @@ function ExercisePage() {
 
   return (
     <main className="bg-background h-screen pt-16 px-4">
-      <h1 className="text-xl font-bold text-primary">
-        {exercise.name.split(" ").map(Capitalize).join(" ")}
-      </h1>
-      <p className="text-secondary/75 mt-2 text-sm">{exercise.date}</p>
-      <p className="text-secondary/75 mt-2 text-sm">{exercise.time}</p>
-      <textarea
-        className="text-secondary/75 mt-2 text-sm pr-5 bg-transparent w-full outline-none"
-        value={exercise.description}
-        onChange={(e) =>
-          setExercise({ ...exercise, description: e.target.value })
-        }
-      ></textarea>
-      <h2 className="text-primary/80 text-lg font-semibold mt-8">Workouts</h2>
+      {exercise === null ? (
+        <CgDisc
+          className="animate-spin text-primary/80 mx-auto mt-32"
+          size={40}
+        />
+      ) : (
+        <>
+          <h1 className="text-xl font-bold text-primary">
+            {exercise.name.split(" ").map(Capitalize).join(" ")}
+          </h1>
+          <div className="flex gap-2">
+            <p className="text-secondary/75 mt-2 text-sm">{exercise.date}</p>
+            <p className="text-secondary/75 mt-2 text-sm">{exercise.time}</p>
+          </div>
+          <textarea
+            className="text-secondary/75 mt-2 text-sm pr-5 bg-transparent w-full outline-none"
+            value={exercise.description}
+            onChange={(e) =>
+              setExercise({ ...exercise, description: e.target.value })
+            }
+          ></textarea>
+          <h2 className="text-primary/80 text-lg font-semibold mt-8">
+            Workouts
+          </h2>
+        </>
+      )}
     </main>
   );
 }
