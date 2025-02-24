b7fdc4c588b53f29da236b53c09734a212d6dca4
fuerte <leandrofuerte75@gmail.com>
2025-02-24 11:03:34 +0100

    Styling workout card

--- src/components/workout-card.tsx
+++ src/components/workout-card.tsx
@@ -12,7 +12,7 @@ function WorkOutCard({ date }: Props) {
   const monthName = month < 12 ? Months[month - 1] : "Invalid month";
 
   return (
-    <li className="flex flex-col border-2 border-primary/80 justify-center items-center h-12 rounded-md shadow-xl shadow-button/50">
+    <li className="flex flex-col justify-center border-[1px] border-primary/20 items-center h-12 rounded-md shadow-lg shadow-button/20">
       <p className="font-semibold text-primary/80 leading-none">{day}</p>
       <p className="font-semibold text-primary/80 leading-none">
         {monthName.slice(0, 3)}
