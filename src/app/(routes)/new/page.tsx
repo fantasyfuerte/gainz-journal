c8c7814da38f055d54b06ab2c1e8df0a3b3f22ce
fuerte <leandrofuerte75@gmail.com>
2025-03-03 06:51:34 +0100

    Responsive fixed in page /new

--- src/app/(routes)/new/page.tsx
+++ src/app/(routes)/new/page.tsx
@@ -2,7 +2,7 @@ import NewExerciseForm from "@/components/new-exercise-form";
 
 function NewExercisePage() {
   return (
-    <main className="h-screen bg-background pt-20 px-8">
+    <main className="h-screen bg-background pt-20 px-8 md:px-60 lg:px-80">
       <NewExerciseForm />
     </main>
   );
