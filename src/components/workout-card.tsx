d413d5e548c95d08b5e34daecf30dfc20e56b3f4
fuerte <leandrofuerte75@gmail.com>
2025-03-03 06:59:47 +0100

    max width added in workout card

--- src/components/workout-card.tsx
+++ src/components/workout-card.tsx
@@ -11,7 +11,7 @@ function WorkOutCard({ training }: Props) {
   const monthName = Months[Number(month) - 1];
 
   return (
-    <li className="flex flex-col justify-center border-[1px] border-primary/20 items-center h-12 rounded-md shadow-lg shadow-button/20">
+    <li className="flex flex-col justify-center border-[1px] border-primary/20 items-center h-12 rounded-md shadow-lg shadow-button/20 max-w-20">
       <p className="font-semibold text-primary/80 leading-none">{day}</p>
       <p className="font-semibold text-primary/80 leading-none">
         {monthName.slice(0, 3)}
