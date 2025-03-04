6fcbd65d1f8ee5756d11962f043feaac052d1096
fuerte <leandrofuerte75@gmail.com>
2025-03-04 11:39:51 +0100

    Conditional rednering due to modal state

--- src/components/workouts-list.tsx
+++ src/components/workouts-list.tsx
@@ -16,15 +16,25 @@ function WorkoutsList({ trainings }: Props) {
 
   return (
     <article className="min-h-28">
-      <h2 className="text-primary/90 text-lg font-semibold mt-8">Workouts</h2>
-      {trainings == null ? (
-        <p className="text-primary/80 text-center mt-4">No trainings found</p>
+      {isModalOpen ? (
+        <div></div>
       ) : (
-        <ul className="p-2 grid grid-cols-5 gap-2">
-          {trainings?.map((training) => (
-            <WorkOutCard key={training.id} training={training} />
-          ))}
-        </ul>
+        <>
+          <h2 className="text-primary/90 text-lg font-semibold mt-8">
+            Workouts
+          </h2>
+          {trainings == null ? (
+            <p className="text-primary/80 text-center mt-4">
+              No trainings found
+            </p>
+          ) : (
+            <ul className="p-2 grid grid-cols-5 gap-2">
+              {trainings?.map((training) => (
+                <WorkOutCard setIsModalOpen={setIsModalOpen} key={training.id} training={training} />
+              ))}
+            </ul>
+          )}
+        </>
       )}
     </article>
   );
