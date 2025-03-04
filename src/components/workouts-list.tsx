13451624a24edc26aa2e0b7a0e2bd601a62d8b8f
fuerte <leandrofuerte75@gmail.com>
2025-03-04 11:29:07 +0100

    Creating isModalOpen state

--- src/components/workouts-list.tsx
+++ src/components/workouts-list.tsx
@@ -1,4 +1,5 @@
 import WorkOutCard from "@/components/workout-card";
+import { useState } from "react";
 
 export type Training = {
   id: number;
@@ -11,12 +12,15 @@ interface Props {
 }
 
 function WorkoutsList({ trainings }: Props) {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <article className="min-h-28">
       <h2 className="text-primary/90 text-lg font-semibold mt-8">Workouts</h2>
       {trainings == null ? (
         <p className="text-primary/80 text-center mt-4">No trainings found</p>
-      ) : (
+      ) : (s
         <ul className="p-2 grid grid-cols-5 gap-2">
           {trainings?.map((training) => (
             <WorkOutCard key={training.id} training={training} />
