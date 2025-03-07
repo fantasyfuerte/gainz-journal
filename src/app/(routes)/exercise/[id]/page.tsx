bf9b22d624f6ab6d0926dfca1f7b594bff828ff3
fuerte <leandrofuerte75@gmail.com>
2025-03-07 12:08:38 +0100

    Quitting chart from page

--- src/app/(routes)/exercise/[id]/page.tsx
+++ src/app/(routes)/exercise/[id]/page.tsx
@@ -13,7 +13,6 @@ import { type Exercise } from "@/components/exercise-list";
 import { Capitalize } from "@/libs/utils";
 import WorkoutsList from "@/components/workouts-list";
 import AddWorkOutModal from "@/components/add-workout-modal";
-import Chart from "@/components/chart";
 import { Training } from "@prisma/client";
 
 function ExercisePage() {
@@ -95,9 +94,6 @@ function ExercisePage() {
             trainings={trainings}
             exerciseId={id}
           />
-          {trainings && trainings.length >= 2 && (
-            <Chart trainings={trainings} />
-          )}
           <ul className="flex gap-2 flex-wrap mt-8">
             <button
               className="bg-button text-lg text-primary font-bold rounded-lg py-2 px-4 self-end"
