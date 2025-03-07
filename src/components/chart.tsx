91f74bff4c1dfdae477d7d41f1a2b4cb48a5619f
fuerte <leandrofuerte75@gmail.com>
2025-03-07 12:04:45 +0100

    Adding base chart

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,11 +1,40 @@
 import { Training } from "@prisma/client";
+import { VictoryArea, VictoryChart, VictoryTheme } from "victory";
 
 interface Props {
   trainings: Training[];
 }
 
 function Chart({ trainings }: Props) {
-  return <div className="text-red-600 text-xl">Chart</div>;
+  if (trainings.length < 2)
+    return <div className="text-red-600 text-xl">No enough data</div>;
+
+  return (
+    <div className="">
+      <VictoryChart theme={VictoryTheme.clean}>
+        <VictoryArea
+          data={[
+            {
+              x: 1,
+              y: 1,
+            },
+            {
+              x: 2,
+              y: 3,
+            },
+            {
+              x: 3,
+              y: 1,
+            },
+            {
+              x: 4,
+              y: 2,
+            },
+          ]}
+        />
+      </VictoryChart>
+    </div>
+  );
 }
 
 export default Chart;
