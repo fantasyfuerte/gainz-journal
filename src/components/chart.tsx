53d7164ed3aea904318de49fd4e674df53e59790
fuerte <leandrofuerte75@gmail.com>
2025-03-08 15:32:52 +0100

    Customizing tick style

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,4 +1,4 @@
-import { VictoryArea } from "victory";
+import { VictoryArea, VictoryChart, VictoryAxis } from "victory";
 
 interface Props {
   data: { maxweight: number; date: Date }[];
@@ -6,35 +6,50 @@ interface Props {
 
 function Chart({ data }: Props) {
   const chartData = data.map((data) => ({
-    x: new Date(data.date).toLocaleDateString(),
+    x: new Date(data.date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+    }),
     y: data.maxweight,
   }));
 
   return (
     <div className="max-w-96 mx-auto">
-      <VictoryArea
-        data={chartData}
-        interpolation={"natural"}
-        labels={({ datum }) => datum.y}
-        style={{
-          parent: {
-            zIndex: 0,
-          },
-          data: {
-            fill: "#7f5af0",
-            fillOpacity: 0.4,
-            stroke: "#7f5af0",
-            strokeWidth: 4,
-            zIndex: 1,
-          },
-          labels: {
-            fontSize: 15,
-            fill: "#7f5af0",
-            zIndex: 1,
-            fontWeight: 500,
-          },
-        }}
-      />
+      <VictoryChart>
+        <VictoryAxis
+          crossAxis
+          style={{
+            tickLabels: {
+              fontSize: 15,
+              fill: "#7f5af0",
+              fontWeight: 500,
+            },
+          }}
+        />
+        <VictoryArea
+          data={chartData}
+          interpolation={"natural"}
+          labels={({ datum }) => datum.y}
+          style={{
+            parent: {
+              zIndex: 0,
+            },
+            data: {
+              fill: "#7f5af0",
+              fillOpacity: 0.4,
+              stroke: "#7f5af0",
+              strokeWidth: 4,
+              zIndex: 1,
+            },
+            labels: {
+              fontSize: 15,
+              fill: "#7f5af0",
+              zIndex: 1,
+              fontWeight: 500,
+            },
+          }}
+        />
+      </VictoryChart>
     </div>
   );
 }
