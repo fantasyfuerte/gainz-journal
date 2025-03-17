93c44ac360476915702fa5f5342a77c97669d1fc
fuerte <leandrofuerte75@gmail.com>
2025-03-17 16:58:29 +0100

    Tyling signin bu¨tton

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -2,8 +2,8 @@ import { HandleSignIn } from "@/libs/authActions";
 
 export default function SignIn() {
   return (
-    <form action={HandleSignIn}>
-      <button type="submit">Signin with Google</button>
+    <form action={HandleSignIn} className="px-3 py-2 rounded-lg bg-button text-lg font-bold text-primary flex justify-center mx-10 font-sans">
+      <button type="submit">Sign In</button>
     </form>
   );
 }
