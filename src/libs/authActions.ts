e6d788b9b56cd880d116647c10567a70287af940
fuerte <leandrofuerte75@gmail.com>
2025-03-18 06:44:10 +0100

    Creating async auth function

--- src/libs/authActions.ts
+++ src/libs/authActions.ts
@@ -1,7 +1,12 @@
 "use server";
 
-import { signIn } from "@/auth";
+import { auth, signIn } from "@/auth";
 
 export async function HandleSignIn() {
   await signIn("google");
+}
+
+export async function Auth() {
+  const session = await auth();
+  return session;
 }
