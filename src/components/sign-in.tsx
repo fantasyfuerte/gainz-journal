487a5ed62cab05759dcb431217e8807edd91034f
fuerte <leandrofuerte75@gmail.com>
2025-03-18 06:13:27 +0100

    Styling signin button

--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -2,8 +2,13 @@ import { HandleSignIn } from "@/libs/authActions";
 
 export default function SignIn() {
   return (
-    <form action={HandleSignIn} className="px-3 py-2 rounded-lg bg-button text-lg font-bold text-primary flex justify-center mx-10 font-sans">
-      <button type="submit">Sign In</button>
+    <form
+      action={HandleSignIn}
+      className=" text-lg font-bold text-primary flex mx-10 font-sans mt-2"
+    >
+      <button type="submit" className="rounded-lg bg-button px-3 py-2 grow">
+        Sign In
+      </button>
     </form>
   );
 }
