fb7bd966bc2cfe1d358d0d7d769cceb187ac4893
fuerte <leandrofuerte75@gmail.com>
2025-03-18 06:10:51 +0100

    Adding new exercise button tonavbar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,17 +2,14 @@ import { CgHomeAlt } from "react-icons/cg";
 import MenuButton from "./menu-button";
 import Link from "next/link";
 import ExerciseList from "./exercise-list";
+import NewExercise from "./new-exercise-link";
 
 async function NavBar() {
   return (
     <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary bg-background z-50">
       <MenuButton>
         <div className="flex flex-col gap-3 py-3 pb-10 px-2 h-full overflow-y-scroll">
-          <Link href="/new" className="flex flex-col">
-            <button className="bg-button rounded-lg py-2 my-3">
-              New Exercise
-            </button>
-          </Link>
+          <NewExercise />
           <ExerciseList />
         </div>
       </MenuButton>
