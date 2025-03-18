8c186f2159ef1a16b4163d80550099e7232119ae
fuerte <leandrofuerte75@gmail.com>
2025-03-18 06:09:47 +0100

    Componentizing new exercise button

---
+++ src/components/new-exercise-link.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Link from "next/link";
+import { useContext } from "react";
+import { UserContext } from "@/context/userProvider";
+
+function NewExercise() {
+  const user = useContext(UserContext).user;
+
+  if (user == null) return <></>;
+
+  return (
+    <Link href="/new" className="flex flex-col">
+      <button className="bg-button rounded-lg py-2 my-3">New Exercise</button>
+    </Link>
+  );
+}
+
+export default NewExercise;
