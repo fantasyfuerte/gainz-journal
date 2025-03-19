f15d5b350d6a0ec05280ea69e44638e82fff52f6
fuerte <leandrofuerte75@gmail.com>
2025-03-19 12:47:23 +0100

    Turning h5 into button

--- src/components/sign-out.tsx
+++ src/components/sign-out.tsx
@@ -10,15 +10,15 @@ function SignOut() {
   if (user == null) return <></>;
 
   return (
-    <h5
+    <button
       onClick={() => {
         refresh();
         HandleSignOut();
       }}
-      className="text-secondary absolute bottom-0 py-2 text-center font-medium text-lg mx-auto hover:opacity-65"
+      className="text-secondary absolute bottom-0 py-2 font-medium text-lg mx-auto hover:opacity-65"
     >
       Sign Out
-    </h5>
+    </button>
   );
 }
 
