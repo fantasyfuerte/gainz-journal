5045cf860d62116cc16ae4eaf2d80279177d1632
Fuerte <leandrofuerte75@gmail.com>
2025-04-18 12:58:53 -0400

    Refactor ExercisePage and DataProvider to integrate useData context and streamline refresh logic

--- src/context/dataProvider.tsx
+++ src/context/dataProvider.tsx
@@ -23,12 +23,16 @@ export const DataContext = createContext<DataContext>({
 
 export function DataProvider({ children }: { children: ReactNode }) {
   const [exercises, setExercises] = useState<null | Exercise[]>(null);
-  const [refreshTrigger, setRefreshTrigger] = useState(false);
 
   const { user } = useContext(UserContext);
 
   function refresh() {
-    setRefreshTrigger((prev) => !prev);
+    if (user === null || user.email === undefined || user.email === null)
+      return;
+    loadExercises(user.email).then((data) => {
+      if (data.message === "No exercises found") return [];
+      return data.sort((a: Exercise, b: Exercise) => b.id - a.id);
+    }).then(setExercises);
   }
 
   useEffect(() => {
@@ -40,7 +44,7 @@ export function DataProvider({ children }: { children: ReactNode }) {
         return data.sort((a: Exercise, b: Exercise) => b.id - a.id);
       })
       .then(setExercises);
-  }, [refreshTrigger, user]);
+  }, [user]);
 
   return (
     <DataContext.Provider value={{ exercises: exercises, refresh: refresh }}>
