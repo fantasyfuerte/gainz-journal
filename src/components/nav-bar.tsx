9dab30f11143bd15204e4bcd59d47cdac9e1fa9b
Fuerte <leandrofuerte75@gmail.com>
2025-04-18 18:32:55 -0400

    Remove background color from NavBar component

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -7,7 +7,7 @@ import SignOut from "./ui/sign-out";
 
 async function NavBar() {
   return (
-    <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary bg-background z-50">
+    <nav className="flex justify-between items-center p-4 fixed w-full top-0 text-primary z-50">
       <MenuButton>
         <div className="flex flex-col gap-3 py-3 pb-10 px-2 h-full overflow-y-scroll">
           <NewExercise />
